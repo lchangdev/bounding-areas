72731d7f337436fd61483061b2c04d2db5a2fd78
Lawrence Chang <lc.uclakers@gmail.com>
2014-06-07 14:12:56 -0400

    All tests passing

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,24 @@
+require 'pry'
 class BoundingArea
+
+  attr_reader :areas
+  def initialize(areas)
+    @areas = areas
+
+  end
+
+  def contains_point?(x, y)
+    results = []
+
+    areas.each do |a|
+      if x >= a.left && x <= a.right && y >= a.bottom && y <= a.top
+        results << true
+      else
+        results << false
+      end
+    end
+
+    results.include?(true)
+  end
+
 end
